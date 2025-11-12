5e6ec02dc21bc564d6f9641c7dc19e174fb3f12d
Cristina <cristinarme@educastur.org>
2025-11-12 11:57:16 +0100

    Actualizando saludo por segunda vez

--- src/com/endes/ut3/saludo.java
+++ src/com/endes/ut3/saludo.java
@@ -4,7 +4,7 @@ public class saludo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola Github desde Eclipse");
+		System.out.println("Hola Github desde Eclipse otra vez");
 	}
 
 }
